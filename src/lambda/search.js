b51e5fc4ee631426eaa59202703bba357ce97e36
Travis Horn <travis@travishorn.com>
2018-12-09 09:41:29 -0600

    Move db. Normalize query.

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,25 +1,23 @@
-const db = [
-  {
-    name: 'sugar',
-    level: 1,
-    type: 'ingredient',
-    explanation: 'Sugar is not easily processed by a diabetic person\'s body. Too much sugar can cause high blood sugar (hyperglycemia) which can cause increased thirst, frequent urination, fatigue, nausea and vomiting, shortness of breath, stomach pain, fruity breath odor, a very dry mouth, rapid heartbeat, and diabetic coma.',
-    suggestions: 'Sugar is important for bodily function, but sugar intake should be carefully monitored to avoid high blood sugar (hyperglycemia) and low blood sugar (hypoglycemia).',
-  },
-];
+const { singular } = require('pluralize');
+const { foods } = require('../database.json');
 
 exports.handler = (event, context, callback) => {
-  const { q } = event.queryStringParameters;
+  const q = singular(event.queryStringParameters.q.toLowerCase());
 
-  const found = db.filter(food => food.name === q);
+  const matches = foods.filter(food => food.name === q);
 
   const results = {
-    resultCount: found.length,
-    results: found,
+    query: event.queryStringParameters.q,
+    interpretation: q,
+    resultCount: matches.length,
+    results: matches,
   };
 
   callback(null, {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(results),
   });
 };
