f0dcb2ab731168c6b979c85d0748106cba6f0f25
Travis Horn <travis@travishorn.com>
2018-12-10 10:20:15 -0600

    Theme color change

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="search">
     <div class="form-group">
       <label for="search" class="bmd-label-floating">Search for a food or ingredient</label>
-      <input id="search" class="form-control" type="text" v-model="q" autofocus>
+      <input id="search" class="form-control" type="text" v-model="q">
     </div>
   </form>
 </template>
@@ -20,6 +20,16 @@ export default {
       this.$emit('search', this.q);
     },
   },
+  mounted() {
+    /*
+     * The search input should automatically be focused when loading the app. However, the floating
+     * labels on Bootstrap Material Design don't work with autofocus. We must wait a short timeout
+     * before focusing on the input
+     */
+    setTimeout(() => {
+      document.getElementById('search').focus();
+    }, 100);
+  }
 };
 </script>
 
