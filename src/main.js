2ddcb16e08a8334865bc78701277c322d2967433
Travis Horn <travis@travishorn.com>
2018-12-09 20:45:54 -0600

    Add PWA support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
