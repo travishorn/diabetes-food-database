bbc89457b419d5f36aa4cf28179b1ba0c75927ab
Travis Horn <travis@travishorn.com>
2018-12-14 17:25:02 -0600

    Remove BMD JS. Make search input bigger

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,8 @@
 <template>
   <form @submit.prevent="search">
     <div class="form-group">
-      <label for="search" class="bmd-label-floating">Search for a food or ingredient</label>
-      <input id="search" class="form-control" type="text" v-model="q">
+      <input id="search" class="form-control" type="text" v-model="q"
+             placeholder="Search for a food" autofocus>
     </div>
   </form>
 </template>
@@ -19,20 +19,16 @@ export default {
     search() {
       this.$emit('search', this.q);
     },
-  },
-  mounted() {
-    /*
-     * The search input should automatically be focused when loading the app. However, the floating
-     * labels on Bootstrap Material Design don't work with autofocus. We must wait a short timeout
-     * before focusing on the input
-     */
-    setTimeout(() => {
-      document.getElementById('search').focus();
-    }, 100);
   }
 };
 </script>
 
 <style scoped>
+  .bmd-label-static {
+    color: #555;
+  }
 
+  .form-control {
+    font-size: 2rem;
+  }
 </style>
